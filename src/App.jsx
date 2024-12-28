4d8b0d261d62fccdb6f2901ad46665b35fc3a307
Rakibul-rahi <rakibulislam.rahi.rir@gmail.com>
2024-12-28 18:04:51 +0600

    about us and contact us pages are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import BaseLayout from './views/BaseLayout';
 import Home from './views/Home';
 import AvailableCats from './views/AvailableCats';
 
+import ContactUs from './views/ContactUs';
+import AboutUs from './views/AboutUs';
+
+
+
 function App() {
   return (
     <Routes>
@@ -15,7 +20,9 @@ function App() {
       >
         <Route path={'/'} element={<Home />} />
         <Route path={'/available-cats'} element={<AvailableCats />} />
-      </Route>
+        <Route path={'/about-us'} element={<AboutUs />} />
+        <Route path={'/contact-us'} element={<ContactUs />} />
+        </Route>
     </Routes>
   );
 }
---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+const ContactUs = () => {
+    return (
+      <div className="About-us">
+        <h2>About Us</h2>
+        <p>If you have any questions, feel free to reach out to us!</p>
+      </div>
+    );
+  };
+  
+  export default ContactUs;
